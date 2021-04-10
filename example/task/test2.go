01b388ea92ef21ca3afae82989bc3c67ea23c207
kongzhenlong <kongzhenlong@hetao101.com>
2021-04-10 20:28:46 +0800

    fix: go mod name

--- example/task/test2.go
+++ example/task/test2.go
@@ -3,7 +3,7 @@ package task
 import (
 	"context"
 	"fmt"
-	xxl "github.com/xxl-job/xxl-job-executor-go"
+	xxl "github.com/konglong87/xxl-job-executor-go"
 	"time"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/xxl-job/xxl-job-executor-go
+module github.com/konglong87/xxl-job-executor-go
 
 go 1.14
 
