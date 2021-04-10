979cb93bddff25b00542a462c9afa61435896cff
kongzhenlong <kongzhenlong@hetao101.com>
2021-04-10 18:00:31 +0800

    init: my-job

--- example/main.go
+++ example/main.go
@@ -11,7 +11,7 @@ func main() {
 	exec := xxl.NewExecutor(
 		xxl.ServerAddr("http://127.0.0.1/xxl-job-admin"),
 		xxl.AccessToken(""),            //请求令牌(默认为空)
-		xxl.ExecutorIp("127.0.0.1"),    //可自动获取
+		//xxl.ExecutorIp("127.0.0.1"),    //可自动获取
 		xxl.ExecutorPort("9999"),       //默认9999（非必填）
 		xxl.RegistryKey("golang-jobs"), //执行器名称
 		xxl.SetLogger(&logger{}),       //自定义日志
